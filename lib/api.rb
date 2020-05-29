5c0f5509d8501305d055a98ef32ee2a7e668cdae
jcmcarth06 <jcmcarth06@gmail.com>
2020-05-28 19:11:20 -0600

    changed objects, calls, user promt/ input

--- lib/api.rb
+++ lib/api.rb
@@ -1,56 +1,36 @@
-
-class API
-  # make calls to our API 
-  # what I write will guide my 
-  # properties an will be used as
-  # attr_accessors in anatomical class
-
-  def self.search_index(searchValue)
-
+class API 
+  
+  def self.search_index
     url = 'http://stapi.co/api/v1/rest/species/search'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
-    array_of_species = hash["species"] #iterate so i can look at one species at a time; call species.new 
+    array_of_species = hash["species"] #iterate so i can look at one species at a time; call species.new
                                         # "birthing a species from the factory", save it as a word "species_instance"
                                     # call setter method to name quadrant/ properties, etc.
-                                    # iterate over array_of_species for hash, then iteate over that internal hash in th array 
+                                    # iterate over array_of_species for hash, then iteate over that internal hash in th array
                                     # |key, value|, add every true value into species_instace
-    array_of_properties.each do |properties_hash|
-      property_instance = Property.new
-                
+    array_of_species.each do |species_hash|
+      species_instance = Species.new
+      species_instance.extinctSpecies = species_hash["extinctSpecies"]
+      species_instance.name = species_hash["name"]
+      species_instance.warpCapableSpecies = species_hash["warpCapableSpecies"]
+      species_instance.extraGalacticSpecies = species_hash["extraGalacticSpecies"]
+      species_instance.humanoidSpecies = species_hash["humanoidSpecies"]
+      species_instance.reptilianSpecies = species_hash["reptilianSpecies"]
+      species_instance.nonCorporealSpecies = species_hash["nonCorporealSpecies"]
+      species_instance.shapeshiftingSpecies = species_hash["shapeshiftingSpecies"]
+      species_instance.spaceborneSpecies = species_hash["spaceborneSpecies"]
+      species_instance.telepathicSpecies = species_hash["telepathicSpecies"]
+      species_instance.transDimensionalSpecies = species_hash["transDimensionalSpecies"]
+      species_instance.unnamedSpecies = species_hash["unnamedSpecies"]
+      species_instance.alternateReality = species_hash["alternateReality"]
     end
   end
-
-   
-  # def self.buildarray_of_(properties_hash)
-  #   properties = array.new(12) #limit number of loops to seach the properties list
-  #   properties.length.times do |i|
-  #     if species_hash["extinctSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["extinctSpecies" + i.to_s]
-  #       species_hash["warpCapableSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["warpCapableSpecies" + i.to_s]
-  #       species_hash["extraGalacticSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["extraGalacticSpecies" + i.to_s]
-  #       species_hash["humanoidSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["humanoidSpecies" + i.to_s]
-  #       species_hash["reptilianSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["reptilianSpecies" + i.to_s]
-  #         species_hash["nonCorporealSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["nonCorporealSpecies" + i.to_s]
-  #       species_hash["shapeshiftingSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["shapeshiftingSpecies" + i.to_s]
-  #       species_hash["spaceborneSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["spaceborneSpecies" + i.to_s]
-  #       species_hash["telepathicSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["telepathicSpecies" + i.to_s]
-  #       species_hash["transDimensionalSpecies" + i.to_s] != false
-  #         properties[i] = species_hash["transDimensionalSpecies" + i.to_s]
-  #       species_hash["alternateReality" + i.to_s] != false
-  #         properties[i] = species_hash["alternateReality" + i.to_s]
-  #     end
-  #   end
-  # end
-  
-
+  def self.searchSpecies(input)
+    return Species.all.select {|s| s.name.downcase.include? input.downcase}
+  end
+  def self.displaySpecies(arr)
+    arr.each { |x| puts "warpCapableSpecies? #{x.warpCapableSpecies}" }
+  end
 end
--- lib/species.rb
+++ lib/species.rb
@@ -1,19 +1,12 @@
 class Species
-
   @@all = []
-    attr_accessor :name, :quadrant, :properties
-  # attr_accessor :extinctSpecies, :warpCapableSpecies, :extraGalacticSpecies, :humanoidSpecies, :reptilianSpecies, :nonCorporealSpecies, :shapeshiftingSpecies, :spaceborneSpecies, :telepathicSpecies, :transDimensionalSpecies, :alternateReality 
+  attr_accessor :extinctSpecies, :warpCapableSpecies, :extraGalacticSpecies, :humanoidSpecies, :reptilianSpecies, :nonCorporealSpecies, :shapeshiftingSpecies, :spaceborneSpecies, :telepathicSpecies, :transDimensionalSpecies, :alternateReality, :unnamedSpecies, :name
   # keep track of the species list and attributes
-  def initialize 
+  def initialize
     @@all << self
-    @properties = []
   end
-
   def self.all
     @@all
   end
-
-
-
 end
 
